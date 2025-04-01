7bc0a20131b911c4a9a56f187dd14e874d65139c
josesanchez79 <ironjo04@gmail.com>
2025-04-01 02:11:07 -0600

    added src, main, textnode, unittests and scripts for main and tests

---
+++ src/test_textnode.py
@@ -0,0 +1,27 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+        
+    
+    def test_noteq(self):
+        node3 = TextNode("This is a text node", TextType.BOLD)
+        node4 = TextNode("This is not a text node", TextType.BOLD)
+        self.assertNotEqual(node3, node4)
+
+    def test_isNone(self):
+        node5 = TextNode("This is not a text node", TextType.ITALIC)
+        self.assertIsNone(node5.url)
+
+    def test_isProperty(self):
+        node6 = TextNode("this is?", TextType.ITALIC)
+        self.assertIsInstance(node6.text_type, TextType)
+
+if __name__ == "__main__":
+    unittest.main()
