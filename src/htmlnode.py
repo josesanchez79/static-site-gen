18e4f791bb2122f9611a68e149c0f72a032d8209
josesanchez79 <ironjo04@gmail.com>
2025-04-01 02:55:32 -0600

    developing html node, in progress

---
+++ src/htmlnode.py
@@ -0,0 +1,22 @@
+class HTMLNode():
+    def __init__(self, tag = None, value = None, children = None, props = None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+    
+    def to_html(self):
+        raise NotImplementedError()
+    
+    def props_to_html(self):
+        if self.props is None:
+            return ""
+        
+        prop_string = ""
+        for prop in self.props:
+            value = self.props[prop]
+            prop_string += f' {prop}="{value}"'
+        return prop_string
+    
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
